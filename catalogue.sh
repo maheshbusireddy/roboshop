af3f87fc53c329ecb64a91bfc0cc2859aed7b607
maheshbusireddy <busireddymahesh@gmail.com>
2023-06-23 19:50:56 +0530

    added domains

--- catalogue.sh
+++ catalogue.sh
@@ -3,7 +3,7 @@ script_location=$(pwd)
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
-mkdir /app 
+mkdir -p /app 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
 rm -rf /app/*
 cd /app 
