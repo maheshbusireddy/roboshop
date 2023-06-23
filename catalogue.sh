75e05de1641048b373188445c35e4c1275cefffe
maheshbusireddy <busireddymahesh@gmail.com>
2023-06-23 21:15:28 +0530

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -2,10 +2,9 @@ script_location=$(pwd)
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
-#useradd roboshop
+useradd roboshop
 mkdir -p /app 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
-rm -rf /app/*
 cd /app 
 unzip /tmp/catalogue.zip
 cd /app 
